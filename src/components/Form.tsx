85858f3f18de84019024cd365c99f4dfb66abf97
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-08-24 16:25:47 +0800

    implement schema-based validation using zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,24 @@
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type FormData = {
-  name: string;
-  age: number;
-};
+const schema = z.object({
+  name: z
+    .string({ message: "The name field is required" })
+    .min(3, { message: "The name must be at least 3 characters." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "Age must be at least 18" }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -23,17 +31,12 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="name"
           className="form-control"
           type="text"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger">The name is required.</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">The name must be at least 3 characters.</p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
 
       <div className="mb-3">
@@ -41,11 +44,12 @@ const Form = () => {
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           className="form-control"
           type="number"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
 
       <button className="btn btn-primary" type="submit">
